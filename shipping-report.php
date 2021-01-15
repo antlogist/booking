fa30237190ec5ebd58a27061007471a299a79844
antlogist <antlogist@gmail.com>
2021-01-15 21:35:25 +0300

    Report json encode

--- shipping-report.php
+++ shipping-report.php
@@ -7,10 +7,12 @@ function build_report() {
   $sql = "SELECT id, name, phone, email, date, address, timeslot FROM bookings";
   $result = mysqli_query($mysqli, $sql);
   
+  $rows = array();
+  
   if (mysqli_num_rows($result) > 0) {
     // output data of each row
     while($row = mysqli_fetch_assoc($result)) {
-      echo "id: " . $row["id"]. " - Name: " . $row["name"] . "<br>";
+      $rows[] = $row;
     }
   } else {
     echo "0 results";
@@ -18,8 +20,46 @@ function build_report() {
 
   mysqli_close($mysqli);
   
+  $rows = json_encode($rows);
+  
+  return $rows;
+  
 }
 
-build_report();
-
 ?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <div id="shippingReport"></div>
+
+    <!-- jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+    
+    <script>
+      (function reportRengder() {
+        const reportData = <?php echo build_report(); ?>;
+        
+        
+        
+      })();
+    </script>
+    
+  </body>
+</html>
+
+
+
+
+
