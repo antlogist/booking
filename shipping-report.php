03c1407f70e8b33492d8091be4d04bc45232acb3
antlogist <antlogist@gmail.com>
2021-01-16 12:38:43 +0300

    Pagination button rendering added

--- shipping-report.php
+++ shipping-report.php
@@ -38,35 +38,29 @@ function build_report() {
 
   <!-- Bootstrap CSS -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+  
+  <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
 
   <title>Hello, world!</title>
 </head>
 
 <body>
   <style>
-/*
-    table {
-      width: 100%;
-      table-layout: fixed;
-    }
-
-    table td {
-      width: 100%;
-    }
-*/
     #shippingReport table {
       font-size: 14px;
     }
+
     #shippingReport table tr td {
       vertical-align: middle;
     }
-    
-    .table-hover tbody tr:hover td, .table-hover tbody tr:hover th {
+
+    .table-hover tbody tr:hover td,
+    .table-hover tbody tr:hover th {
       background-color: #9fc51c;
     }
 
   </style>
-  <div class="container">
+  <div class="container my-5">
     <div id="shippingReport"></div>
   </div>
 
@@ -77,68 +71,107 @@ function build_report() {
 
 
   <script>
-    (function reportRender() {
-      const reportData = <?php echo build_report(); ?>;
-
-      if (!reportData.length) {
-        return;
+    (function report() {
+      
+      const data = <?php echo build_report(); ?>;
+      let currentPageNumber = 1;
+      const itemsPerPage = 3;
+      const totalNumbersOfPages = Math.ceil(Number(data.length) / itemsPerPage);
+      
+      reportRender(data.slice(0, itemsPerPage));
+      
+      
+      function changePage(page) {
+        
+        const currentPageData = data.slice(0, 2);
+        console.log(currentPageData);
+        reportRender(currentPageData);
       }
+      
 
-      // DOM fragment element
-      const fragment = document.createDocumentFragment();
+      function reportRender(data) {
+        const reportData = data;
 
-      // Table
-      const table = document.createElement("table");
-      table.classList.add("table", "table-striped", "table-bordered", "table-hover", "table-sm", "table-responsive-md");
+        if (!reportData.length) {
+          return;
+        }
 
-      // Head
-      const thead = document.createElement("thead");
-      //      thead.classList.add("thead-dark");
-      const theadTr = document.createElement("tr");
-      thead.appendChild(theadTr);
+        // DOM fragment element
+        const fragment = document.createDocumentFragment();
+        
+        // Pagination buttons
+        
+        const paginationWrapper = document.createElement("div");
+        paginationWrapper.classList.add("text-center", "my-3");
+        
+        const buttonPrev = document.createElement("button");
+        buttonPrev.classList.add("btn", "btn-sm", "mr-1", "btn-prev");
+        buttonPrev.textContent = "Prev";
+        const buttonNext = document.createElement("button");
+        buttonNext.classList.add("btn", "btn-sm", "ml-1", "btn-next");
+        buttonNext.textContent = "Next";
+        
+        const iconPrev = document.createElement("i");
+        iconPrev.classList.add("fa", "fa-angle-double-left", "mr-1");
+        buttonPrev.insertAdjacentElement("afterbegin", iconPrev);
+        
+        const iconNext = document.createElement("i");
+        iconNext.classList.add("fa", "fa-angle-double-right", "ml-1");
+        buttonNext.insertAdjacentElement("beforeend", iconNext);
+        
+        paginationWrapper.appendChild(buttonPrev);
+        paginationWrapper.appendChild(buttonNext);
 
-      // Th
-      const thArr = Object.keys(reportData[0]);
-      thArr.map((item) => {
-        const th = document.createElement("th");
-        th.setAttribute("scope", "col");
-        th.textContent = item;
-        theadTr.appendChild(th);
-      })
+        // Table
+        const table = document.createElement("table");
+        table.classList.add("table", "table-striped", "table-bordered", "table-hover", "table-sm", "table-responsive-md");
 
-      // Body
-      const tbody = document.createElement("tbody");
-      reportData.map((item) => {
-        const tr = document.createElement("tr");
-        const values = Object.values(item);
-        values.map((value, index, arr) => {
-          const td = document.createElement("td");
-          
-//          if (arr.length === index+1) {
-//              console.log(arr.length, index);
-//          } 
-          
-          td.textContent = arr.length === index+1 ? "pending" : value;
-          tr.appendChild(td);
-        });
-        tbody.appendChild(tr);
-      })
+        // Head
+        const thead = document.createElement("thead");
+        //      thead.classList.add("thead-dark");
+        const theadTr = document.createElement("tr");
+        thead.appendChild(theadTr);
 
-      // Append fragment
-      table.appendChild(thead);
-      table.appendChild(tbody);
-      fragment.appendChild(table);
-      document.getElementById("shippingReport").appendChild(fragment);
+        // Th
+        const thArr = Object.keys(reportData[0]);
+        thArr.map((item) => {
+          const th = document.createElement("th");
+          th.setAttribute("scope", "col");
+          th.textContent = item;
+          theadTr.appendChild(th);
+        })
+
+        // Body
+        const tbody = document.createElement("tbody");
+        reportData.map((item) => {
+          const tr = document.createElement("tr");
+          const values = Object.values(item);
+          values.map((value, index, arr) => {
+            const td = document.createElement("td");
+
+            td.textContent = arr.length === index + 1 ? "pending" : value;
+            tr.appendChild(td);
+          });
+          tbody.appendChild(tr);
+        })
+
+        // Append fragment
+        table.appendChild(thead);
+        table.appendChild(tbody);
+        fragment.appendChild(table);
+        fragment.appendChild(paginationWrapper);
+        document.getElementById("shippingReport").appendChild(fragment);
+      }
+      
+      const shippingReport = document.getElementById("shippingReport");
+      shippingReport.addEventListener("click", reportClick);
+      
+      function reportClick(e) {
+        const target = e.target;
+        console.log(target);
+      }
+      
     })();
-    
-//    (function events() {
-//      const el = document.getElementById("shippingReport");
-//      el.addEventListener("mouseover", tableHover);
-//      function tableHover(e) {
-//        const target = e.target;
-//        console.log(target);
-//      }
-//    })();
 
   </script>
 
